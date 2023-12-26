b371bbdbf6c8c0ee3778d01c6679fc7456853412
Sam Marks <sam@sammarks.me>
2023-12-26 15:04:59 -0500

    feat: upgrade to node 20

--- feature-server-deploy/push.sh
+++ feature-server-deploy/push.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env sh
 
+VERSION="$1"
+if [[ -z "$VERSION" ]]; then
+  echo "must pass version"
+  exit 1
+fi
+
 set -e
 
 docker build . -t thesisedu/feature-server-deploy:build
-docker tag thesisedu/feature-server-deploy:build ghcr.io/thesisedu/docker/feature-server-deploy:latest
-docker push ghcr.io/thesisedu/docker/feature-server-deploy:latest
+docker tag thesisedu/feature-server-deploy:build ghcr.io/thesisedu/docker/feature-server-deploy:$VERSION
+docker push ghcr.io/thesisedu/docker/feature-server-deploy:$VERSION
